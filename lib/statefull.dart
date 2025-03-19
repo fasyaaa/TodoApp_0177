4ca53633116d83e2886e6faf074ee5f15eb8c2e2
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-03-19 09:12:18 +0700

    ADD - Implement basic StatefulWidget structure in Flutter

---
+++ lib/statefull.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Statefull extends StatefulWidget {
+  const Statefull({super.key});
+
+  @override
+  State<Statefull> createState() => _StatefullState();
+}
+
+class _StatefullState extends State<Statefull> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
